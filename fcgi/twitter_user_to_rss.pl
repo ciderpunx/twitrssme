a79530ece1e09e318ab581f005b6394f8d766af7
Théo Le Calvar <tlc@kher.nl>
2018-09-26 21:20:58 +0200

    strip image attached with emojis

--- fcgi/twitter_user_to_rss.pl
+++ fcgi/twitter_user_to_rss.pl
@@ -109,6 +109,7 @@ while (my $q = CGI::Fast->new) {
       $body=~s{target="_blank"}{}gi;
       $body=~s{</?s[^>]*>}{}gi;
       $body=~s{data-[\w\-]+="[^"]+"}{}gi; # validator doesn't like data-aria markup that we get from twitter
+      $body=~s{<img.*?alt="(.+?)"[^>]*>}{$1}gi;
       my $avatar = $header->findvalue('./img' . class_contains("avatar") . "/\@src"); 
       my @fst_img_a = $tweet->findnodes( './div//div'
                                        . class_contains("js-adaptive-photo")
